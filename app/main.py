1be01cf361f80ed48eb80fb03cb97b770eadb57d
FaryanAmini <faryanlife@gmail.com>
2026-03-20 14:27:19 -0400

    added yolo functions to main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import json
 from fastapi import FastAPI, File, Form, HTTPException, UploadFile
 from pydantic import BaseModel
 
+from . import yolo
+
 # import the projection class projection.py
 from .projection import GroundProjection
 
@@ -51,11 +53,16 @@ async def rec_telemetry(
 
     # collect binary bytes for the image
     image_bytes = await image.read()
-
+    detections = yolo.detect(image_bytes)
+    print(f"Detections: {detections}")
     # save image_bytes into a file or database
     # with open("saved_image.jpg", "wb") as f:
     #     f.write(image_bytes)
 
     # returning just the telemetry data like the original Rust code
     # return HttpResponse::Ok().json(data);
-    return data
+    return {
+        "telemetry": data,
+        "projection": ground_projection,
+        "detections": detections,
+    }
