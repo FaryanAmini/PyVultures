8b5e8ad2f35f14e1d30e59241252afe556782eb4
FaryanAmini <faryanlife@gmail.com>
2026-04-27 11:08:04 -0400

    added function outline to toMesh in the 3D engine

--- 3DRecon/toMesh.py
+++ 3DRecon/toMesh.py
@@ -1,3 +1,5 @@
+from sys import path
+
 import numpy as np
 import open3d as o3d
 
@@ -8,3 +10,21 @@ mesh = o3d.io.read_triangle_mesh(target_mesh)
 # extract verticies to numpy array
 points = np.asarray(mesh.verticies)
 print(f"loaded {len(points)} points from glb")
+
+# function to create mesh
+
+
+def generate_mesh(input_path, output_path):
+    pcd = o3d.io.read_point_cloud(input_path)
+    if not pcd.has_points():
+        mesh = o3d.io.read_triangle_mesh(input_path)
+        pcd.points = mesh.vertices
+        pcd.colors = mesh.vertex_colors
+
+    print(f"loaded {len(pcd.points)} points from {input_path}")
+
+    # remove statistical outliers
+
+    # estimate normals
+
+    # mesh reconstruction
